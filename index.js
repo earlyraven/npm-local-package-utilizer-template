5319fa6d07a2d0bfc64ce2996c9eadf12103738d
Nicolas Jager <nicowebsitemail@gmail.com>
2023-10-17 02:10:43 -0400

    Improve error handling and include linking directions for 'MODULE_NOT_FOUND' errors.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const {greetMe} = require("npm-local-package-template");
+const packageName = "npm-local-package-template";
 
-greetMe("Mr. Smith");
+try {
+  const { greetMe } = require(packageName);
+  greetMe("Mr. Smith");
+} catch (error) {
+  console.log(error);
+
+  if (error.code === "MODULE_NOT_FOUND") {
+    _provideLinkingInstructions(packageName);
+  }
+
+  process.exit(1); // Exit with an error code
+}
+
+function _provideLinkingInstructions(packageName) {
+  const supplementalMessage = `${packageName} may not be linked or installed into node_modules properly.
+To link:
+cd into package_folder:
+npm link
+cd into project_folder_that_uses_package:
+npm link ${packageName}
+
+To unlink:
+cd into project_folder_that_uses_package:
+npm unlink ${packageName}
+cd into package_folder:
+npm unlink
+`;
+  console.error(supplementalMessage);
+}
